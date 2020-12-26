78ab042c8d7647ad89b389b4b3a7b826261d5273
Lewis Alderman <lewisalderman95@gmail.com>
2020-12-26 23:11:09 +0000

    Some style tweaks to scroll-to-top btn

--- pages/index.js
+++ pages/index.js
@@ -69,11 +69,11 @@ function Homepage(props) {
       
       <Main>
 
-        {/* SCROLL UP */}
+        {/* SCROLL UP BTN */}
         {isScrollToTopVisible && (
-          <button className="fixed z-10 w-12 h-12 px-2 py-1 text-center rounded-full opacity-20 bg-blueGray-200 text-blueGray-500 right-3 bottom-3 hover:opacity-100"
+          <button className='fixed z-10 w-12 h-12 px-2 py-1 text-center rounded-full opacity-50 bg-blueGray-200 text-blueGray-500 right-3 bottom-3 hover:opacity-100 lg:w-20 lg:h-20 scrollToTopBtn'
           onClick={() => typeof document !== 'undefined' && (document.documentElement.scrollTop = 0)}>
-            <svg className="w-8 h-8" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6 4H18V6H6zM11 14L11 20 13 20 13 14 18 14 12 8 6 14z"></path></svg>
+            <svg className="w-8 h-8 mx-auto fill-current lg:w-12 lg:h-12" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6 4H18V6H6zM11 14L11 20 13 20 13 14 18 14 12 8 6 14z"></path></svg>
           </button>
         )}
         

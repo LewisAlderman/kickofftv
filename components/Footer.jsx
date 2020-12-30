1a2e371ce992f1b5cd9771563b07543ab4648783
Lewis Alderman <lewisalderman95@gmail.com>
2020-12-30 16:06:24 +0000

    Netlify badge in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,18 @@ import { PROD } from "../constants.ts";
 export default function Footer() {
 	return (
 		<footer className="grid py-2 bg-emerald-400 text-emerald-900 h-28 place-items-center pb-safe-bottom">
-			{PROD() ? 'Footer' : <span><a href="https://github.com/LewisAlderman/televised-football" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://app.netlify.com/sites/naughty-jang-12b423/overview" target="_blank" rel="noopener noreferrer">Netlify</a></span>}
+			{
+			PROD()
+			? 'Footer'
+			: <span className="flex items-center">
+				<a href="https://github.com/LewisAlderman/televised-football" target="_blank" rel="noopener noreferrer" className="mx-3">
+					Github
+				</a> | <a href="https://app.netlify.com/sites/kickofftv/deploys" target="_blank" rel="noopener noreferrer" className="inline-block mx-3">
+					<img src="https://api.netlify.com/api/v1/badges/dd2e281e-aaae-4491-a9d7-61fa3ac01594/deploy-status" alt="Netlify"/>
+				</a>
+			</span>
+			}
+			
 		</footer>
 	)
 }

d0c26ff4da3eeda7bee40315271aa50938384cfc
Lewis Alderman <lewisalderman95@gmail.com>
2020-12-30 20:00:19 +0000

    WIP footer changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,19 +1,25 @@
 import { PROD } from "../constants.ts";
+import PageWrapper from "./PageWrapper";
 
 export default function Footer() {
 	return (
-		<footer className="grid py-2 bg-emerald-400 text-emerald-900 h-28 place-items-center pb-safe-bottom">
-			{
-			PROD()
-			? 'Footer'
-			: <span className="flex items-center">
-				<a href="https://github.com/LewisAlderman/televised-football" target="_blank" rel="noopener noreferrer" className="mx-3">
+		<footer className="flex items-center justify-center h-48 py-2 bg-emerald-400 text-emerald-800 pb-safe-bottom">
+			
+			<PageWrapper className="flex flex-col items-center justify-center space-y-2">
+			<div className="mb-2 text-sm text-center text-emerald-600">
+				All data for this site is curated for education purposes only
+			</div>
+			
+			<div className="flex items-center">
+				<a href="https://github.com/LewisAlderman/televised-football" target="_blank" rel="noopener noreferrer" className="mx-3 text-xs font-bold uppercase">
 					Github
-				</a> | <a href="https://app.netlify.com/sites/kickofftv/deploys" target="_blank" rel="noopener noreferrer" className="inline-block mx-3">
+				</a>
+				<span className="text-emerald-500">|</span>
+				<a href="https://app.netlify.com/sites/kickofftv/deploys" target="_blank" rel="noopener noreferrer" className="inline-block mx-3">
 					<img src="https://api.netlify.com/api/v1/badges/dd2e281e-aaae-4491-a9d7-61fa3ac01594/deploy-status" alt="Netlify"/>
 				</a>
-			</span>
-			}
+			</div>
+			</PageWrapper>
 			
 		</footer>
 	)

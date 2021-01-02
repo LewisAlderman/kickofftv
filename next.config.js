e6620beccc7b83afd5ca6cacb507b0441391202b
Lewis Alderman <lewisalderman95@gmail.com>
2021-01-02 22:43:10 +0000

    Prettier + eslint + husky + lint-staged

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const dev = process.env.NODE_ENV === 'development';
 
 const config = {
-	reactStrictMode: true,
-	poweredByHeader: false,
-	poweredByHeader: false,
-	target: 'serverless' // for netlify
-}
+  reactStrictMode: true,
+  poweredByHeader: false,
+  target: 'serverless', // for netlify
+};
 
-module.exports = dev 
-? config
-: withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-	},
-	...config,
-})
+module.exports = dev
+  ? config
+  : withPWA({
+      pwa: {
+        dest: 'public',
+        runtimeCaching,
+      },
+      ...config,
+    });

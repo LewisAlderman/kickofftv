cde27d8f2193bc1fc7e671f3cfe52a0243943d25
Lewis Alderman <lewisalderman95@gmail.com>
2021-01-03 21:51:12 +0000

    Moved StatsBubble to its own comp

---
+++ components/StatsBubble.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const StatsBubble = ({ groups, postponedMatchCount }) => {
+  return (
+    <div className="relative z-20 flex justify-end">
+      <div className="absolute py-6 pl-8 pr-12 rounded bg-blueGray-300 text-blueGray-300 stats-bubble">
+        <ul className="space-y-2 text-blueGray-700">
+          {!!groups?.gender?.male?.length && (
+            <li>
+              <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
+                {groups.gender.male.length}
+              </span>
+              <span>game(s) mens</span>
+            </li>
+          )}
+          {!!groups?.gender?.female?.length && (
+            <li>
+              <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
+                {groups.gender.female.length}
+              </span>
+              <span>game(s) womens</span>
+            </li>
+          )}
+          {!!groups?.youth?.true?.length && (
+            <li>
+              <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
+                {groups.youth.true.length}
+              </span>
+              <span>game(s) youth players</span>
+            </li>
+          )}
+          {!!postponedMatchCount && (
+            <li>
+              <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
+                {postponedMatchCount}
+              </span>
+              <span>game(s) postponed</span>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default StatsBubble;
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import Filters from '@components/Filters';
 import ScrollUpButton from '@components/ScrollUpButton';
 import ScrollDownButton from '@components/ScrollDownButton';
 import SVG from '@components/SVG';
+import StatsBubble from '@components/StatsBubble';
 
 /**
  * @param {Object} props
@@ -110,7 +111,7 @@ function Homepage({ data, lastUpdated, postponedMatchCount }) {
 
                 <button
                   className="relative flex items-end px-10 py-5 space-x-3 text-sm cursor-pointer -right-10"
-                  onClick={() => setBubbleVisible(!isBubbleVisible)}>
+                  onPointerDown={() => setBubbleVisible(!isBubbleVisible)}>
                   <span className="flex items-center text-teal-500">
                     <span className="mr-1">
                       <SVG.Eye />
@@ -132,44 +133,10 @@ function Homepage({ data, lastUpdated, postponedMatchCount }) {
             </div>
 
             {isBubbleVisible && (
-              <div className="relative z-20 flex justify-end">
-                <div className="absolute py-6 pl-8 pr-12 rounded bg-blueGray-300 text-blueGray-300 stats-bubble">
-                  <ul className="space-y-2 text-blueGray-700">
-                    {!!groups?.gender?.male?.length && (
-                      <li>
-                        <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
-                          {groups.gender.male.length}
-                        </span>
-                        <span>game(s) mens</span>
-                      </li>
-                    )}
-                    {!!groups?.gender?.female?.length && (
-                      <li>
-                        <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
-                          {groups.gender.female.length}
-                        </span>
-                        <span>game(s) womens</span>
-                      </li>
-                    )}
-                    {!!groups?.youth?.true?.length && (
-                      <li>
-                        <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
-                          {groups.youth.true.length}
-                        </span>
-                        <span>game(s) youth players</span>
-                      </li>
-                    )}
-                    {!!postponedMatchCount && (
-                      <li>
-                        <span className="inline-flex items-center justify-center w-12 py-0.5 mr-2 font-medium font-mono text-sm rounded-full text-blueGray-200 bg-blueGray-600">
-                          {postponedMatchCount}
-                        </span>
-                        <span>game(s) postponed</span>
-                      </li>
-                    )}
-                  </ul>
-                </div>
-              </div>
+              <StatsBubble
+                groups={groups}
+                postponedMatchCount={postponedMatchCount}
+              />
             )}
 
             {/* SCROLL DOWN */}
